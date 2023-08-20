aaec105c091ec4a05828c8a4ad5fda4b47afe04a
Lucas Fernando <lucasf.geron@icloud.com>
2023-08-20 10:30:36 -0300

    lets que lets

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -16,6 +16,30 @@ module.exports = {
       },
       spacing: {
         '18': '4.5rem',
+        '30': '7.5rem',
+        '42': '10.5rem',
+        '54': '13.5rem',
+        '86': '21.5rem',
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        breathe: 'breathe 3s ease-in-out infinite',
+        notify: 'notify 3s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        breathe: {
+          '0%, 100%': { transform: 'scale(1.0)' },
+          '50%': { transform: 'scale(1.02)' },
+        },
+        notify: {
+          '0%, 33.33%': { transform: 'scale(0.8)', opacity: 0 },
+          '33.33%, 66.66%': { transform: 'scale(1)', opacity: 1 },
+          '66.66%, 100%': { transform: 'scale(0.8)', opacity: 0 },
+        }
       }
     },
   },
